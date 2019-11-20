9d6c5d49ebde3d163c815d1d5c7520609da677e8
italo041 <bitalofernando@gmail.com>
2019-11-20 18:32:14 -0500

    Se realizo unos cambios

--- lib/src/pages/estadisticas_page.dart
+++ lib/src/pages/estadisticas_page.dart
@@ -1,4 +1,4 @@
-import 'package:appriego/src/pages/charts.dart';
+
 import 'package:appriego/src/pages/estadisticas_humedad_page.dart';
 import 'package:appriego/src/pages/time_series_chart.dart';
 import 'package:appriego/src/pages/uso_agua_page.dart';
@@ -16,20 +16,6 @@ class EstadisticasPage extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
-          ListTile(
-            title: Text("Charts"),
-            leading: Icon(Icons.timelapse,color: Colors.lightGreen[400],),
-            trailing: Icon(Icons.keyboard_arrow_right,color:Colors.lightGreen[400]),
-            onTap: (){
-               
-
-              final route = MaterialPageRoute(
-                builder: (context) =>  CharBar(),
-              );
-              Navigator.push(context, route);
-            },
-          ),
-          Divider(),
           ListTile(
             title: Text("Temperatura del ambiente"),
             leading: Icon(Icons.timelapse,color: Colors.lightGreen[400],),
